d6839caeecf119e18ea964263ebb26cda5343b30
AS-Azevedo <as.azevedo7@gmail.com>
2021-07-30 16:32:11 -0300

    Números de 1 a 100

---
+++ 1-100.c
@@ -0,0 +1,25 @@
+/*Escreva um programa que exibe os numeros de 1 a 100*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	
+	int i;
+	
+	for(i = 1; i <= 100; i++) 
+	{
+		
+		printf("%d\n", i);
+	
+	}
+	
+	
+	
+	
+	
+	system("Pause");
+	
+	return 0;
+}
