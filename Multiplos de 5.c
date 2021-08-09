4f6554edd8d3c6972822b9f2c0f495a79c24b41f
AS-Azevedo <as.azevedo7@gmail.com>
2021-08-09 07:37:36 -0300

    Multiplos de 5

---
+++ Multiplos de 5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+	
+	int i;
+	
+	for (i = 0; i<=500; i++)
+{
+      if (i%5 == 0)
+      printf ("%d\n", i);
+}
+		
+	
+	system("Pause");
+	return 0;
+}
